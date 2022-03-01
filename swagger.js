c93256853af3ff1181fa5b92d50eb80a7112232f
dylan <dylan_zeng92@qq.com>
2022-03-01 14:05:01 +0800

    feat: swagger update

--- swagger.js
+++ swagger.js
@@ -24,42 +24,6 @@ main({
   port: 9000,
 });
 
-// const main = require('@dylan92/nemo');
-// // const fetch = require('node-fetch')
-// const { generateColumns } = require('sandfish');
-
-// // 业务系统环境映射
-// const envMap = {
-//   test03: 'test03',
-//   test05: 'test05',
-//   dev: 'dev',
-//   stage: 'stage',
-// };
-// // swagger的环境
-// const env = 'test03';
-
-// main({
-//   // remote swagger api json
-//   // url: 'remote',
-//   // requestPath: '@/utils/request',
-//   url: 'https://petstore.swagger.io/v2/swagger.json',
-//   // output path
-//   output: './src/api',
-// });
-
-// // // 根据数据库生成前端用的列配置
-// const config = {
-//   connectionConfig: {
-//     host: 'localhost',
-//     user: 'root',
-//     password: '949440946',
-//   },
-//   dataBaseNames: ['DYLAN'], // 数据库名字
-//   outputPath: './sql', // 输出目录
-//   ignoreTableNames: ['database_history'], // 不生成表格表单配置的黑名单
-//   needFormConfig: true, // 是否需要生成表单配置， 默认关闭
-// };
-// generateColumns(config);
 // // 根据字典生成前端ts字典
 
 // const { generateEnums } = require('enum-maker');
